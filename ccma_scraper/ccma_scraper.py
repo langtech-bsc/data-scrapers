fe7cc332a4b7e2ed48514cbdacf2d010aa9c34a7
Abir Messaoudi <37579275+AbirMessaoudi@users.noreply.github.com>
2026-04-02 15:51:15 +0200

    Output one TSV per entry with proper handling of missing data

--- ccma_scraper/ccma_scraper.py
+++ ccma_scraper/ccma_scraper.py
@@ -15,62 +15,99 @@ def die(msg=""):
     sys.exit(1)
 
 def main():
-    if not (2 <= len(sys.argv) <= 3):
+    if not (2 <= len(sys.argv) <= 4):
         die()
     datafile = sys.argv[1]
-    num      = sys.argv[2]
-
+    
+    
     with open(datafile, 'r') as file:
         jsondata = json.load(file)
     all_data = pd.DataFrame(jsondata["docs"])
 
     # use only rows in Catalan, without mp4_1200_es and in HQ
     nonan_data=all_data.fillna("nan")
-    clean_data = nonan_data[(nonan_data["idioma"]=="Català") & (nonan_data["mp4_1200_es"]=="nan") & (nonan_data["ebuttd_ca"]!="nan") & (nonan_data["mp4_hq"]!="nan")].drop_duplicates(subset=["mp4_hq"])
+    clean_data = nonan_data[(nonan_data["idioma"]=="Català") & (nonan_data["mp4_1200_es"]=="nan") & (nonan_data["ebuttd_ca"]!="nan")].drop_duplicates(subset=["mp4_hq"])
 
-    data = clean_data[["id", "mp4_hq", 'ebuttd_ca', "durada_segons"]]
-    if num:
-        data=data[:int(num)]
-    else:
+    data = clean_data[["id", "mp4_hq", "mp4_mq", 'ebuttd_ca', "durada_segons"]]
+    try:
+        num = sys.argv[3]    
+    except:    
         num = len(clean_data)
 
-    print(f"Extracting {str(num)} videos from {datafile}")
+    try:
+        numz = sys.argv[2]
+    except:
+        numz = 0
+    
+    data=data[int(numz):int(num)]
+
+    print(f"Extracting {str(len(data))} videos from {datafile}")
 
     def create_new_id(video_string):
         name = video_string.split("/")[-1]
         return name.split(".")[0]
 
     def create_audioname(new_id):
-        return "audios/" + new_id + ".wav"
-
+        return "ingestion/" + new_id + ".wav"
+        
     def create_transcriptions(url_transcrip):
         try:
             soup = BeautifulSoup(urlopen(url_transcrip).read(),'html.parser')
+            return " ".join([item.text for item in soup.find_all('tt:span')])
         except:
             print(url_transcrip)
-        return " ".join([item.text for item in soup.find_all('tt:span')])
+            return "@# error in transcription: " + url_transcrip
 
     data["new_id"] = data.apply(lambda x: create_new_id(x.mp4_hq), axis=1)
-    data["transcription"] = data.apply(lambda x: create_transcriptions(x.ebuttd_ca), axis=1)
-    data["audiofile"] = data.apply(lambda x: create_audioname(x.new_id), axis=1)
-
-    print("downloading videos")
+    data["text"] = data.apply(lambda x: create_transcriptions(x.ebuttd_ca), axis=1)
+    data["wav_path"] = data.apply(lambda x: create_audioname(x.new_id), axis=1)
+    
+    data[data["text"].str.contains("@# error in transcription: ")].to_csv("error_in_transcription.tsv", sep="\t")    
+    data_to_download = data[~data["text"].str.contains("@# error in transcription: ")]
+    
+    print(f"Creating data folders...")
     os.makedirs("videos", exist_ok=True)
-    for v in tqdm(data["mp4_hq"].to_list()):
-        wget.download(v, out="videos")
-
-    print("extracting audio")
     os.makedirs("audios", exist_ok=True)
-    for v in tqdm(data["new_id"].to_list()):
-        videoname = v + ".mp4"
-        audioname = v + ".wav"
-        extract_audio(input_path="videos/" + videoname, output_path="audios/" + audioname, overwrite=True, output_format="wav")
-
     os.makedirs("fsp_tsv", exist_ok=True)
-    output_name = "fsp_tsv/ccma_" + str(date.today()) + ".tsv"
-    data[["audiofile", "transcription"]].to_csv(output_name, sep="\t")
 
-    print(f"Data saved in {output_name}")
+    videos_hq = data_to_download["mp4_hq"].to_list()
+    videos_mq = data_to_download["mp4_mq"].to_list()
+    new_ids = data_to_download["new_id"].to_list()
+    wav_paths = data_to_download["wav_path"].to_list()
+    transcriptions = data_to_download["text"].to_list()
 
+    errors_list = []
+        
+    for i, new_id in enumerate(new_ids):
+        if i % 100 == 0:
+            print(f"Downloaded {i} videos")
+            pd.DataFrame(errors_list).to_csv("not_downloaded_data_" + str(i).zfill(5) + ".tsv", sep="\t")
+        try:
+            video_url = videos_hq[i]
+            wget.download(video_url, out="videos")
+        except:
+            try:
+                video_url = videos_mq[i]
+                wget.download(video_url, out="videos")
+            except:
+                errors_list.append([new_id, "cannot download video"])
+                
+        try:
+            
+            videoname = new_id + ".mp4"
+            audioname = new_id + ".wav"
+            extract_audio(input_path="videos/" + videoname, output_path="audios/" + audioname, overwrite=True, output_format="wav")
+        except:
+            errors_list.append([new_id, "cannot extract audio"])
+
+        try:
+            filename = new_id + ".tsv"
+            pd.DataFrame([[wav_paths[i], transcriptions[i]]], columns=["wav_path", "text"]).to_csv("fsp_tsv/" + filename, sep="\t", index=False)
+        except:
+            errors_list.append([new_id, "cannot save transcriptions"])
+    
+    print(f"Data downloaded!")
+    pd.DataFrame(errors_list).to_csv("not_downloaded_data.tsv", sep="\t")
+    print(pd.DataFrame(errors_list))
 if __name__ == "__main__":
     main()
